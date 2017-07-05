3d5c6a51895665476f72e6fdf52302cd2c720b13
俞俞 <hanabiranoshitaniumore@gmail.com>
2017-07-05 22:16:52 +0800

    加入專案檔案。

---
+++ Viyrex.Extend/Data/MappingProvider(T).cs
@@ -0,0 +1,67 @@
+﻿namespace System.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using System.Reflection;
+    public class MappingProvider<T>
+    {
+        internal MappingProvider(DataTable table)
+        {
+            this.Table = table;
+            this.RelationShip = new Dictionary<string, MemberInfo>();
+        }
+
+        private readonly IDictionary<string, MemberInfo> RelationShip;
+
+        private readonly DataTable Table;
+
+        /// <summary>
+        /// 讓欄位與物件欄位產生關連
+        /// </summary>
+        /// <typeparam name="TMappingField"></typeparam>
+        /// <param name="columnName">欄位名稱</param>
+        /// <param name="selector">物件欄位選擇器</param>
+        /// <returns></returns>
+        public MappingProvider<T> MapTo(string columnName, Expression<Func<T, object>> selector)
+        {
+            if (this.Table.Columns.Cast<DataColumn>().All(x => x.ColumnName != columnName)) throw new ArgumentException(nameof(columnName));
+            var member = (selector.Body as MemberExpression)?.Member ?? throw new NotSupportedException("Not supported this lambda expression");
+            this.RelationShip.Add(columnName, member);
+            return this;
+        }
+
+        /// <summary>
+        /// 執行關聯作業
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerable<T> Execute(Func<T> instance)
+            => this.Table
+            .AsEnumerable()
+            .Select(row => this.RelationShip.Aggregate(instance(), (obj, rs) => this.AttachValue(obj, rs.Value, row.Field<object>(rs.Key))));
+
+        private T AttachValue(T target, MemberInfo member, object value)
+        {
+            switch (member)
+            {
+                case PropertyInfo p:
+                    p.SetValue(target, value);
+                    break;
+
+                case FieldInfo f:
+                    f.SetValue(target, value);
+                    break;
+            }
+            return target;
+        }
+
+    }
+    public static class MappingExtension
+    {
+        public static MappingProvider<T> Mapping<T>(this DataTable table)
+            => new MappingProvider<T>(table);
+
+    }
+}
---
+++ Viyrex.Extend/Generic/Enum(T).cs
@@ -0,0 +1,89 @@
+﻿namespace System
+{
+    using System.Linq;
+    /// <summary>
+    /// 提供強型別的Enum轉換
+    /// </summary>
+    /// <typeparam name="TEnum"></typeparam>
+    public static class Enum<TEnum> where TEnum : struct, IComparable, IFormattable, IConvertible
+    {
+        public static TEnum[] ResolveAll(TEnum @enum)
+            => Enum<TEnum>.GetValues().Where(x => (@enum as Enum).HasFlag(x as Enum)).ToArray();
+
+        public static TEnum[] Resolve(TEnum @enum)
+            => @enum.ToString().Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries).Select(x => Enum<TEnum>.Parse(x)).ToArray();
+
+        public static string Format<TValue>(TValue value, string format) where TValue : struct, IComparable, IFormattable, IConvertible, IComparable<TValue>, IEquatable<TValue>
+            => Enum.Format(typeof(TEnum), value, format);
+
+        public static string GetName<TValue>(TValue value) where TValue : struct, IComparable, IFormattable, IConvertible, IComparable<TValue>, IEquatable<TValue>
+            => Enum.GetName(typeof(TEnum), value);
+
+        public static string[] GetNames()
+            => Enum.GetNames(typeof(TEnum));
+
+        public static Type GetUnderlyingType()
+            => Enum.GetUnderlyingType(typeof(TEnum));
+
+        public static TEnum[] GetValues()
+            => Enum.GetValues(typeof(TEnum)).Cast<TEnum>().ToArray();
+
+        public static bool IsDefined<TValue>(TValue value) where TValue : struct, IComparable, IFormattable, IConvertible, IComparable<TValue>, IEquatable<TValue>
+            => Enum.IsDefined(typeof(TEnum), value);
+
+        public static TEnum Parse(string value)
+            => (TEnum)Enum.Parse(typeof(TEnum), value);
+
+        public static TEnum Parse(string value, bool ignoreCase)
+            => (TEnum)Enum.Parse(typeof(TEnum), value, ignoreCase);
+
+        public static TEnum ToEnum(int value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum(ulong value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum(byte value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum(uint value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum(short value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum(long value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum(ushort value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum(sbyte value)
+            => Enum<TEnum>.ToEnum(value);
+
+        public static TEnum ToEnum<TValue>(TValue value) where TValue : struct, IComparable, IFormattable, IConvertible, IComparable<TValue>, IEquatable<TValue>
+            => (TEnum)Enum.ToObject(typeof(TEnum), value);
+
+        public static bool TryParse(string value, out TEnum result)
+            => Enum.TryParse<TEnum>(value, out result);
+
+        public static bool TryParse(string value, bool ignoreCase, out TEnum result)
+            => Enum.TryParse<TEnum>(value, ignoreCase, out result);
+    }
+
+    public static class EnumExpansion
+    {
+        public static string Format<TEnum>(this object value, string format) where TEnum : struct, IComparable, IFormattable, IConvertible
+            => Enum.Format(typeof(TEnum), value, format);
+
+        public static string GetName<TEnum>(this object value) where TEnum : struct, IComparable, IFormattable, IConvertible
+            => Enum.GetName(typeof(TEnum), value);
+
+        public static bool IsDefined<TEnum>(this object value) where TEnum : struct, IComparable, IFormattable, IConvertible
+            => Enum.IsDefined(typeof(TEnum), value);
+
+        public static TEnum ToEnum<TEnum>(this object value) where TEnum : struct, IComparable, IFormattable, IConvertible
+            => (TEnum)Enum.ToObject(typeof(TEnum), value);
+
+    }
+}
