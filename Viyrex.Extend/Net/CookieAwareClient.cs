a3f40746d46adfd513ba95a4fb5a706c5ca637e8
俞俞 <hanabiranoshitaniumore@gmail.com>
2017-07-09 14:56:45 +0800

    Add CookieAwareClient

---
+++ Viyrex.Extend/Net/CookieAwareClient.cs
@@ -0,0 +1,22 @@
+﻿namespace System.Net
+{
+    public class CookieAwareClient : WebClient
+    {
+        public CookieAwareClient() : this(null)
+        {
+        }
+
+        public CookieAwareClient(CookieContainer cookieContainer) : base()
+            => this.CookieContainer = cookieContainer ?? new CookieContainer();
+
+        public readonly CookieContainer CookieContainer;
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            var request = base.GetWebRequest(address);
+            if (request is HttpWebRequest castRequest)
+                castRequest.CookieContainer = this.CookieContainer;
+            return request;
+        }
+    }
+}
